44e3bf785809a196319d1ec61c5cac2b2f115f17
BattlestationX2\MJM <MilesJMaiden@Gmail.com>
2023-03-10 15:02:16 +0000

    Jumping Added

--- VRPhysicsLab/Assets/ContinuousMovementPhysics.cs
+++ VRPhysicsLab/Assets/ContinuousMovementPhysics.cs
@@ -7,18 +7,30 @@ public class ContinuousMovementPhysics : MonoBehaviour {
 
     public float speed = 1f;
     public float turnSpeed = 60f;
+    private float jumpVelocity = 7;
+    public float jumpHeight = 1.5f;
     public float snapTurnAngle = 45f;
+
+    public bool onlyMoveWhenGrounded = false;
+    public bool jumpWithHand = true;
+
+    public float minJumpWithHandSpeed = 2f;
+    public float maxJumpWithHandSpeed = 7f;
+
 
     public InputActionProperty moveInputSource;
     public InputActionProperty turnInputSource;
+    public InputActionProperty jumpInputSource;
 
     public Rigidbody rb;
+    public Rigidbody rightHandRB;
+    public Rigidbody leftHandRB;
     public LayerMask groundLayer;
     public Transform directionSource;
     public Transform turnSource;
 
     public CapsuleCollider bodyCollider;
-
+    private bool isGrounded;
 
     private Vector2 inputMoveAxis;
     private float inputTurnAxis;
@@ -28,13 +40,35 @@ public class ContinuousMovementPhysics : MonoBehaviour {
     {
         inputMoveAxis = moveInputSource.action.ReadValue<Vector2>();
         inputTurnAxis = turnInputSource.action.ReadValue<Vector2>().x;
+
+        bool jumpInput = jumpInputSource.action.WasPressedThisFrame();
+
+        if (!jumpWithHand) {
+
+                if(jumpInput && isGrounded)
+            {
+                jumpVelocity = Mathf.Sqrt(2 * -Physics.gravity.y * jumpHeight);
+                rb.velocity = Vector3.up * jumpVelocity;
+            }
+        } else
+        {
+            bool inputJumpPressed = jumpInputSource.action.IsPressed();
+
+            float handSpeed = ((leftHandRB.velocity - rb.velocity).magnitude + (rightHandRB.velocity - rb.velocity).magnitude) / 2;
+
+            if (inputJumpPressed && isGrounded && handSpeed > minJumpWithHandSpeed)
+            {
+                rb.velocity = Vector3.up * Mathf.Clamp(handSpeed, minJumpWithHandSpeed, maxJumpWithHandSpeed);
+            }
+        }
+
     }
 
     void FixedUpdate()
     {
-        bool isGrounded = CheckIfGrounded();
+        isGrounded = CheckIfGrounded();
 
-        if(isGrounded)
+        if(isGrounded || (onlyMoveWhenGrounded && isGrounded))
         {
             Quaternion yaw = Quaternion.Euler(0f, directionSource.eulerAngles.y, 0f);
             Vector3 direction = yaw * new Vector3(inputMoveAxis.x, 0f, inputMoveAxis.y);
